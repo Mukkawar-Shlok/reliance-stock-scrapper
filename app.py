29c0e1acbf2a57dc07a794d4b1a827a4ba11c0a5
Mukkawar-Shlok <shlokmukkawar11@gmail.com>
2024-07-15 20:38:26 +0530

    initial commit

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ def stock_price():
     return jsonify({'stock_price': price})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=1000, debug=True)
+    # app.run(debug=True)
